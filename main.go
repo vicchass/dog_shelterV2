b787a259d0c1e899e8cef52769b2659e98393249
victor Chassaigne <vchassaigne@proton.me>
2023-09-11 17:16:45 -0400

    added 2 main fuction

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"slices"
 
 	u "github.com/vicchass/dog_shelterV2/util"
 )
@@ -14,7 +16,7 @@ var pitbull = u.Breed{Name: "pitbul", Croquette_day: 20, Size: "big", Has_hair: 
 var labrador = u.Breed{Name: "labrador", Croquette_day: 15, Size: "big", Has_hair: true}
 var york = u.Breed{Name: "yorkshire", Croquette_day: 5, Size: "small", Has_hair: true}
 
-// slice of all the Breed struct
+// slice of all the dog struct
 var dog_slice []u.Dog
 
 // declare the dogs that are already in the shelter
@@ -29,13 +31,32 @@ func main() {
 
 	// START LOGIC
 
+	for {
+		fmt.Println("what do you want to do \n a:add a dog \n b:print all dogs \n q:quit")
+		var choice string
+		fmt.Scan(&choice)
+		switch choice {
+		//add a new dog
+		case "a":
+			dog_slice = append(dog_slice, add_dog(dog_slice, breed_slice))
+		//print all dogs
+		case "b":
+			fmt.Println("---------------")
+			print_all_dogs(dog_slice)
+			fmt.Println("---------------")
+		case "q":
+			os.Exit(3)
+		}
+
+	}
+
 }
 
 // END MAIN
 
 // add a new dog to the dogs in shelter
-func add_dog(all_dogs_shelter []u.Dog, the_breed []u.Breed) {
-	// create a name , check dont exist
+func add_dog(all_dogs_shelter []u.Dog, the_breed []u.Breed) u.Dog {
+	// create a name , check if it dont already  exist
 
 	// get all existing name in a slice
 	dogs_names := u.Return_names_dogs(dog_slice)
@@ -43,8 +64,70 @@ func add_dog(all_dogs_shelter []u.Dog, the_breed []u.Breed) {
 	var new_name string
 	for {
 		fmt.Println("what is the name of dog?")
-		fmt.Scan()
+		fmt.Scan(&new_name)
+		if slices.Contains(dogs_names, new_name) {
+			fmt.Println(" name already exist ,choose new one ")
+		} else {
+			break
+		}
+	}
 
+	//type age
+	var new_age uint
+
+	// get the input for the age
+	for {
+		fmt.Println("what is the age of your dog ?")
+		if _, err := fmt.Scan(&new_age); err != nil {
+			fmt.Println("error with your input , try again")
+			continue
+		} else {
+			if new_age > 20 {
+				fmt.Println("Your dog seems very old , type age correctly? y/n")
+				var ans string
+				fmt.Scan(&ans)
+				if ans == "y" {
+					break
+				} else {
+					continue
+				}
+			}
+			break
+		}
+	}
+
+	//choose breed
+
+	breed_names := u.Return_breed_name(the_breed)
+	var breed_input string
+	var out_breed u.Breed
+
+	for {
+		//print all existing breed
+		fmt.Println("what is the breed of your dog?")
+		fmt.Println("the breeds are : ")
+		for _, v := range breed_names {
+			fmt.Println(v)
+		}
+
+		if _, err := fmt.Scan(&breed_input); err != nil {
+			fmt.Println(" error with input try again")
+			continue
+		}
+
+		out_breed = u.Return_breed(the_breed, breed_input)
+		break
+
+	}
+
+	out_dog := u.Dog{The_name: new_name, The_age: new_age, The_breed: out_breed, Is_here: true}
+	return out_dog
+}
+
+// print all the dog in the shelter
+func print_all_dogs(all_dogs []u.Dog) {
+	for _, v := range all_dogs {
+		fmt.Println("we have", v.The_name, " who is a ", v.The_breed.Name)
 	}
 
 }
--- util/util1.go
+++ util/util1.go
@@ -1,9 +1,11 @@
 package util
 
+import "fmt"
+
 //declare the dog struct
 type Dog struct {
 	The_name  string
-	The_age   int
+	The_age   uint
 	The_breed Breed
 	Is_here   bool
 }
@@ -22,5 +24,26 @@ func Return_names_dogs(slice_dogs []Dog) []string {
 		names_slices = append(names_slices, v.The_name)
 	}
 	return names_slices
+}
+
+// return a slice of strings of all breed name
+func Return_breed_name(slice_breed []Breed) []string {
+	var breed_slice_name []string
+	for _, v := range slice_breed {
+		breed_slice_name = append(breed_slice_name, v.Name)
+	}
+	return breed_slice_name
+}
+
+//return a breed type based on its name
+func Return_breed(slice_breed []Breed, the_name string) Breed {
+	for _, v := range slice_breed {
+		if v.Name == the_name {
+			return v
+		}
+	}
+	empty_breed := Breed{}
+	fmt.Println("COULDNT FIND BREED")
+	return empty_breed
 
 }
